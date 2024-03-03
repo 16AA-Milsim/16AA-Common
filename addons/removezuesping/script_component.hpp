f4d82033fb63991aa2e50e24b83c7a6e2e03c38e
Rabbit <Ripperrabbitgaming@gmail.com>
2024-03-03 12:06:15 +0000

    Add Remove Zues Ping

---
+++ addons/removezuesping/script_component.hpp
@@ -0,0 +1,14 @@
+#define COMPONENT removezuesping
+#include "\z\l6AA\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
+#ifdef DEBUG_ENABLED_REMOVEZUESPING
+    #define DEBUG_MODE_FULL
+#endif
+    #ifdef DEBUG_SETTINGS_REMOVEZUESPING
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_REMOVEZUESPING
+#endif
+
+#include "\z\l6AA\addons\main\script_macros.hpp"
